26d3442a0917ffce1d472e47733c5fc0e667a2b1
sleepy9090 <sleepy3d@gmail.com>
2026-04-14 20:27:12 -0400

    Add Win32_Processor vars

--- CPU-S/CPU.cs
+++ CPU-S/CPU.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPU_S
 {
-    internal class CPU
+    internal static class CPU
     {
-        
-
-        public CPU() 
-        { 
-        
-        }
-
         /*
           uint16   AddressWidth;
           uint16   Architecture;
@@ -81,9 +75,7 @@ namespace CPU_S
 
         public static string AssetTag { get; internal set; }
 
-        public static uint Availability { get; internal set; }
-
-        public static uint ExtClock { get; internal set; }
+        public static ushort Availability { get; internal set; }
 
         public static string Caption { get; internal set; }
 
@@ -93,32 +85,103 @@ namespace CPU_S
 
         public static bool ConfigManagerUserConfig { get; internal set; }
 
-        public static uint CpuStatus { get; internal set; }
+        public static ushort CpuStatus { get; internal set; }
 
         public static string CreationClassName { get; internal set; }
 
         public static uint CurrentClockSpeed { get; internal set; }
 
-        public static uint NumberOfCores { get; internal set; }
+        public static ushort CurrentVoltage { get; internal set; }
 
         public static ushort DataWidth { get; internal set; }
 
         public static string Description { get; internal set; }
 
-        public static string ProcessorId { get; internal set; }
+        public static string DeviceID { get; internal set; }
 
-        public static ulong L2CacheSize { get; internal set; }
+        public static bool ErrorCleared { get; internal set; }
 
-        public static ulong L3CacheSize { get; internal set; }
+        public static string ErrorDescription { get; internal set; }
 
-        public static string Name { get; internal set; }
+        public static uint ExtClock { get; internal set; }
 
-        public static string SocketDesignation { get; internal set; }
+        public static uint Family {  get; internal set; }
+
+        public static DateTime InstallDate { get; internal set; }
+
+        public static uint L2CacheSize { get; internal set; }
+
+        public static uint L2CacheSpeed { get; internal set; }
+
+        public static uint L3CacheSize { get; internal set; }
+
+        public static uint L3CacheSpeed { get; internal set; }
+
+        public static uint LastErrorCode { get; internal set; }
+
+        public static ushort Level {  get; internal set; }
+
+        public static ushort LoadPercentage { get; internal set; }
+
+        public static string Manufacturer { get; internal set; }
 
         public static uint MaxClockSpeed { get; internal set; }
 
+        public static string Name { get; internal set; }
+
+        public static uint NumberOfCores { get; internal set; }
+
+        public static uint NumberOfEnabledCore { get; internal set; }
+
         public static uint NumberOfLogicalProcessors { get; internal set; }
 
+        public static string OtherFamilyDescription { get; internal set; }
+
+        public static string PartNumber { get; internal set; }
+
+        public static string PNPDeviceID { get; internal set; }
+
+        public static ushort[] PowerManagementCapabilities { get; internal set; }
+
+        public static bool PowerManagementSupported { get; internal set; }
+
+        public static string ProcessorId { get; internal set; }
+
+        public static ushort ProcessorType { get; internal set; }
+
+        public static ushort Revision { get; internal set; }
+
+        public static string Role { get; internal set; }
+
+        public static bool SecondLevelAddressTranslationExtensions { get; internal set; }
+
+        public static string SerialNumber { get; internal set; }
+
+        public static string SocketDesignation { get; internal set; }
+
+        public static string Status { get; internal set; }
+
+        public static ushort StatusInfo { get; internal set; }
+
+        public static string Stepping {  get; internal set; }
+
+        public static string SystemCreationClassName { get; internal set; }
+
+        public static string SystemName { get; internal set; }
+
+        public static uint ThreadCount { get; internal set; }
+
+        public static string UniqueId { get; internal set; }
+
+        public static ushort UpgradeMethod { get; internal set; }
+
+        public static string Version { get; internal set; }
+
+        public static bool VirtualizationFirmwareEnabled { get; internal set; }
+
+        public static bool VMMonitorModeExtensions { get; internal set; }
+
+        public static uint VoltageCaps { get; internal set; }
 
     }
 }
--- CPU-S/Form1.cs
+++ CPU-S/Form1.cs
@@ -27,36 +27,80 @@ namespace CPU_S
                                    .Cast<ManagementObject>()
                                    .First();
 
-            CPU.ProcessorId = (string)cpu["ProcessorId"];
-            CPU.SocketDesignation = (string)cpu["SocketDesignation"];
-            CPU.Name = (string)cpu["Name"];
-            CPU.Description = (string)cpu["Description"];
             CPU.AddressWidth = (ushort)cpu["AddressWidth"];
-            CPU.DataWidth = (ushort)cpu["DataWidth"];
             CPU.Architecture = (ushort)cpu["Architecture"];
-            CPU.MaxClockSpeed = (uint)cpu["MaxClockSpeed"];
+            CPU.AssetTag = (string)cpu["AssetTag"];
+            CPU.Availability = (ushort)cpu["Availability"];
+            CPU.Caption = (string)cpu["Caption"];
+            CPU.Characteristics = (uint)cpu["Characteristics"];
+            CPU.ConfigManagerErrorCode = (uint)cpu["ConfigManagerErrorCode"];
+            CPU.ConfigManagerUserConfig = (bool)cpu["ConfigManagerUserConfig"];
+            CPU.CpuStatus = (ushort)cpu["CpuStatus"];
+            CPU.CreationClassName = (string)cpu["CreationClassName"];
+            CPU.CurrentClockSpeed = (uint)cpu["CurrentClockSpeed"];
+            CPU.CurrentVoltage = (ushort)cpu["CurrentVoltage"];
+            CPU.DataWidth = (ushort)cpu["DataWidth"];
+            CPU.Description = (string)cpu["Description"];
+            CPU.DeviceID = (string)cpu["DeviceID"];
+            CPU.ErrorCleared = (bool)cpu["ErrorCleared"];
+            CPU.ErrorDescription = (string)cpu["ErrorDescription"];
             CPU.ExtClock = (uint)cpu["ExtClock"];
-            CPU.L2CacheSize = (uint)cpu["L2CacheSize"] * (ulong)1024;
-            CPU.L3CacheSize = (uint)cpu["L3CacheSize"] * (ulong)1024;
+            CPU.Family = (ushort)cpu["Family"];
+            CPU.InstallDate = (DateTime)cpu["InstallDate"];
+            CPU.L2CacheSize = (uint)cpu["L2CacheSize"] * 1024;
+            CPU.L2CacheSpeed = (uint)cpu["L2CacheSpeed"];
+            CPU.L3CacheSize = (uint)cpu["L3CacheSize"] * 1024;
+            CPU.L3CacheSpeed = (uint)cpu["L3CacheSpeed"];
+            CPU.LastErrorCode = (uint)cpu["LastErrorCode"];
+            CPU.Level = (ushort)cpu["Level"];
+            CPU.LoadPercentage = (ushort)cpu["LoadPercentage"];
+            CPU.Manufacturer = (string)cpu["Manufacturer"];
+            CPU.MaxClockSpeed = (uint)cpu["MaxClockSpeed"];
+            CPU.Name = (string)cpu["Name"];
             CPU.NumberOfCores = (uint)cpu["NumberOfCores"];
+            CPU.NumberOfEnabledCore = (uint)cpu["NumberOfEnabledCore"];
             CPU.NumberOfLogicalProcessors = (uint)cpu["NumberOfLogicalProcessors"];
+            CPU.OtherFamilyDescription = (string)cpu["OtherFamilyDescription"];
+            CPU.PartNumber = (string)cpu["PartNumber"];
+            CPU.PNPDeviceID = (string)cpu["PNPDeviceID"];
+            CPU.PowerManagementCapabilities = (ushort[])cpu["PowerManagementCapabilities"];
+            CPU.PowerManagementSupported = (bool)cpu["PowerManagementSupported"];
+            CPU.ProcessorId = (string)cpu["ProcessorId"];
+            CPU.ProcessorType = (ushort)cpu["ProcessorType"];
+            CPU.Revision = (ushort)cpu["Revision"];
+            CPU.Role = (string)cpu["Role"];
+            CPU.SecondLevelAddressTranslationExtensions = (bool)cpu["SecondLevelAddressTranslationExtensions"];
+            CPU.SerialNumber = (string)cpu["SerialNumber"];
+            CPU.SocketDesignation = (string)cpu["SocketDesignation"];
+            CPU.Status = (string)cpu["Status"];
+            CPU.StatusInfo = (ushort)cpu["StatusInfo"];
+            CPU.Stepping = (string)cpu["Stepping"];
+            CPU.SystemCreationClassName = (string)cpu["SystemCreationClassName"];
+            CPU.SystemName = (string)cpu["SystemName"];
+            CPU.ThreadCount = (uint)cpu["ThreadCount"];
+            CPU.UniqueId = (string)cpu["UniqueId"];
+            CPU.UpgradeMethod = (ushort)cpu["UpgradeMethod"];
+            CPU.Version = (string)cpu["Version"];
+            CPU.VirtualizationFirmwareEnabled = (bool)cpu["VirtualizationFirmwareEnabled"];
+            CPU.VMMonitorModeExtensions = (bool)cpu["VMMonitorModeExtensions"];
+            CPU.VoltageCaps = (uint)cpu["VoltageCaps"];
         }
 
         private void PopulateCPUInfo()
         {
-            textBoxCPUProcessorId.Text = CPU.ProcessorId;
-            textBoxCPUSocketDesignation.Text = CPU.SocketDesignation;
-            textBoxCPUName.Text = CPU.Name;
-            textBoxCPUDescription.Text = CPU.Description;
             textBoxCPUAddressWidth.Text = CPU.AddressWidth.ToString();
-            textBoxCPUDataWidth.Text = CPU.DataWidth.ToString();
             textBoxCPUArchitecture.Text = CPU.Architecture.ToString();
-            textBoxCPUMaxClockSpeed.Text = CPU.MaxClockSpeed.ToString();
+            textBoxCPUDataWidth.Text = CPU.DataWidth.ToString();
+            textBoxCPUDescription.Text = CPU.Description;
             textBoxCPUExtClock.Text = CPU.ExtClock.ToString();
             textBoxCPUL2CacheSize.Text = CPU.L2CacheSize.ToString();
             textBoxCPUL3CacheSize.Text = CPU.L3CacheSize.ToString();
+            textBoxCPUMaxClockSpeed.Text = CPU.MaxClockSpeed.ToString();
+            textBoxCPUName.Text = CPU.Name;
             textBoxCPUNumberOfCores.Text = CPU.NumberOfCores.ToString();
             textBoxCPUNumberOfLogicalProcessors.Text = CPU.NumberOfLogicalProcessors.ToString();
+            textBoxCPUProcessorId.Text = CPU.ProcessorId;
+            textBoxCPUSocketDesignation.Text = CPU.SocketDesignation;
         }
     }
 }
