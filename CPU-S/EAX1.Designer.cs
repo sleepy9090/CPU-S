afd02aaab5d53bb2d6dcdf8353da8598cc377026
sleepy9090 <sleepy3d@gmail.com>
2026-08-06 21:23:12 -0400

    EAX1 form

--- CPU-S/EAX1.Designer.cs
+++ CPU-S/EAX1.Designer.cs
@@ -39,11 +39,53 @@
             this.labelEAX1EAX = new System.Windows.Forms.Label();
             this.labelEAX1EAX0_3_SteppingId = new System.Windows.Forms.Label();
             this.textBoxEAX1EAX0_3_SteppingId = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX4_7_ModelId = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX4_7_ModelId = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX8_11_FamilyId = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX8_11_FamilyId = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX12_13_ProcessorType = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX12_13_ProcessorType = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX14_15_Reserved = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX14_15_Reserved = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX16_19_ExtendedModelId = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX16_19_ExtendedModelId = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX_CalculatedProcessorModel = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX_CalculatedProcessorModel = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX20_27_ExtendedFamilyId = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX20_27_ExtendedFamilyId = new System.Windows.Forms.TextBox();
+            this.labelEAX1EAX28_31_Reserved = new System.Windows.Forms.Label();
+            this.textBoxEAX1EAX28_31_Reserved = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labelEAX1EBX0_7_BrandIndex = new System.Windows.Forms.Label();
+            this.textBoxEAX1EBX0_7_BrandIndex = new System.Windows.Forms.TextBox();
             this.groupBoxEAX1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxEAX1
             // 
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EBX0_7_BrandIndex);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EBX0_7_BrandIndex);
+            this.groupBoxEAX1.Controls.Add(this.checkBox1);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX28_31_Reserved);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX28_31_Reserved);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX20_27_ExtendedFamilyId);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX20_27_ExtendedFamilyId);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX_CalculatedProcessorModel);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX_CalculatedProcessorModel);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX16_19_ExtendedModelId);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX16_19_ExtendedModelId);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX14_15_Reserved);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX14_15_Reserved);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX12_13_ProcessorType);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX12_13_ProcessorType);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX8_11_FamilyId);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX8_11_FamilyId);
+            this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX4_7_ModelId);
+            this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX4_7_ModelId);
             this.groupBoxEAX1.Controls.Add(this.labelEAX1EAX0_3_SteppingId);
             this.groupBoxEAX1.Controls.Add(this.textBoxEAX1EAX0_3_SteppingId);
             this.groupBoxEAX1.Controls.Add(this.labelEAX1EDX);
@@ -141,6 +183,176 @@
             this.textBoxEAX1EAX0_3_SteppingId.Size = new System.Drawing.Size(200, 20);
             this.textBoxEAX1EAX0_3_SteppingId.TabIndex = 10;
             // 
+            // labelEAX1EAX4_7_ModelId
+            // 
+            this.labelEAX1EAX4_7_ModelId.AutoSize = true;
+            this.labelEAX1EAX4_7_ModelId.Location = new System.Drawing.Point(6, 152);
+            this.labelEAX1EAX4_7_ModelId.Name = "labelEAX1EAX4_7_ModelId";
+            this.labelEAX1EAX4_7_ModelId.Size = new System.Drawing.Size(53, 13);
+            this.labelEAX1EAX4_7_ModelId.TabIndex = 13;
+            this.labelEAX1EAX4_7_ModelId.Text = "Model ID:";
+            // 
+            // textBoxEAX1EAX4_7_ModelId
+            // 
+            this.textBoxEAX1EAX4_7_ModelId.Location = new System.Drawing.Point(78, 149);
+            this.textBoxEAX1EAX4_7_ModelId.Name = "textBoxEAX1EAX4_7_ModelId";
+            this.textBoxEAX1EAX4_7_ModelId.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX4_7_ModelId.TabIndex = 12;
+            // 
+            // labelEAX1EAX8_11_FamilyId
+            // 
+            this.labelEAX1EAX8_11_FamilyId.AutoSize = true;
+            this.labelEAX1EAX8_11_FamilyId.Location = new System.Drawing.Point(6, 178);
+            this.labelEAX1EAX8_11_FamilyId.Name = "labelEAX1EAX8_11_FamilyId";
+            this.labelEAX1EAX8_11_FamilyId.Size = new System.Drawing.Size(53, 13);
+            this.labelEAX1EAX8_11_FamilyId.TabIndex = 15;
+            this.labelEAX1EAX8_11_FamilyId.Text = "Family ID:";
+            // 
+            // textBoxEAX1EAX8_11_FamilyId
+            // 
+            this.textBoxEAX1EAX8_11_FamilyId.Location = new System.Drawing.Point(78, 175);
+            this.textBoxEAX1EAX8_11_FamilyId.Name = "textBoxEAX1EAX8_11_FamilyId";
+            this.textBoxEAX1EAX8_11_FamilyId.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX8_11_FamilyId.TabIndex = 14;
+            // 
+            // labelEAX1EAX12_13_ProcessorType
+            // 
+            this.labelEAX1EAX12_13_ProcessorType.AutoSize = true;
+            this.labelEAX1EAX12_13_ProcessorType.Location = new System.Drawing.Point(6, 204);
+            this.labelEAX1EAX12_13_ProcessorType.Name = "labelEAX1EAX12_13_ProcessorType";
+            this.labelEAX1EAX12_13_ProcessorType.Size = new System.Drawing.Size(84, 13);
+            this.labelEAX1EAX12_13_ProcessorType.TabIndex = 17;
+            this.labelEAX1EAX12_13_ProcessorType.Text = "Processor Type:";
+            // 
+            // textBoxEAX1EAX12_13_ProcessorType
+            // 
+            this.textBoxEAX1EAX12_13_ProcessorType.Location = new System.Drawing.Point(96, 201);
+            this.textBoxEAX1EAX12_13_ProcessorType.Name = "textBoxEAX1EAX12_13_ProcessorType";
+            this.textBoxEAX1EAX12_13_ProcessorType.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX12_13_ProcessorType.TabIndex = 16;
+            // 
+            // labelEAX1EAX14_15_Reserved
+            // 
+            this.labelEAX1EAX14_15_Reserved.AutoSize = true;
+            this.labelEAX1EAX14_15_Reserved.Location = new System.Drawing.Point(6, 230);
+            this.labelEAX1EAX14_15_Reserved.Name = "labelEAX1EAX14_15_Reserved";
+            this.labelEAX1EAX14_15_Reserved.Size = new System.Drawing.Size(56, 13);
+            this.labelEAX1EAX14_15_Reserved.TabIndex = 19;
+            this.labelEAX1EAX14_15_Reserved.Text = "Reserved:";
+            // 
+            // textBoxEAX1EAX14_15_Reserved
+            // 
+            this.textBoxEAX1EAX14_15_Reserved.Location = new System.Drawing.Point(96, 227);
+            this.textBoxEAX1EAX14_15_Reserved.Name = "textBoxEAX1EAX14_15_Reserved";
+            this.textBoxEAX1EAX14_15_Reserved.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX14_15_Reserved.TabIndex = 18;
+            // 
+            // labelEAX1EAX16_19_ExtendedModelId
+            // 
+            this.labelEAX1EAX16_19_ExtendedModelId.AutoSize = true;
+            this.labelEAX1EAX16_19_ExtendedModelId.Location = new System.Drawing.Point(6, 256);
+            this.labelEAX1EAX16_19_ExtendedModelId.Name = "labelEAX1EAX16_19_ExtendedModelId";
+            this.labelEAX1EAX16_19_ExtendedModelId.Size = new System.Drawing.Size(101, 13);
+            this.labelEAX1EAX16_19_ExtendedModelId.TabIndex = 21;
+            this.labelEAX1EAX16_19_ExtendedModelId.Text = "Extended Model ID:";
+            // 
+            // textBoxEAX1EAX16_19_ExtendedModelId
+            // 
+            this.textBoxEAX1EAX16_19_ExtendedModelId.Location = new System.Drawing.Point(113, 253);
+            this.textBoxEAX1EAX16_19_ExtendedModelId.Name = "textBoxEAX1EAX16_19_ExtendedModelId";
+            this.textBoxEAX1EAX16_19_ExtendedModelId.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX16_19_ExtendedModelId.TabIndex = 20;
+            // 
+            // labelEAX1EAX16_19_ExtendedModelIdLeftShifted
+            // 
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.AutoSize = true;
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.Location = new System.Drawing.Point(6, 282);
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.Name = "labelEAX1EAX16_19_ExtendedModelIdLeftShifted";
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.Size = new System.Drawing.Size(158, 13);
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.TabIndex = 23;
+            this.labelEAX1EAX16_19_ExtendedModelIdLeftShifted.Text = "Extended Model ID Left Shifted:";
+            // 
+            // textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted
+            // 
+            this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted.Location = new System.Drawing.Point(170, 279);
+            this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted.Name = "textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted";
+            this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted.TabIndex = 22;
+            // 
+            // labelEAX1EAX_CalculatedProcessorModel
+            // 
+            this.labelEAX1EAX_CalculatedProcessorModel.AutoSize = true;
+            this.labelEAX1EAX_CalculatedProcessorModel.Location = new System.Drawing.Point(6, 308);
+            this.labelEAX1EAX_CalculatedProcessorModel.Name = "labelEAX1EAX_CalculatedProcessorModel";
+            this.labelEAX1EAX_CalculatedProcessorModel.Size = new System.Drawing.Size(142, 13);
+            this.labelEAX1EAX_CalculatedProcessorModel.TabIndex = 25;
+            this.labelEAX1EAX_CalculatedProcessorModel.Text = "Calculated Processor Model:";
+            // 
+            // textBoxEAX1EAX_CalculatedProcessorModel
+            // 
+            this.textBoxEAX1EAX_CalculatedProcessorModel.Location = new System.Drawing.Point(170, 305);
+            this.textBoxEAX1EAX_CalculatedProcessorModel.Name = "textBoxEAX1EAX_CalculatedProcessorModel";
+            this.textBoxEAX1EAX_CalculatedProcessorModel.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX_CalculatedProcessorModel.TabIndex = 24;
+            // 
+            // labelEAX1EAX20_27_ExtendedFamilyId
+            // 
+            this.labelEAX1EAX20_27_ExtendedFamilyId.AutoSize = true;
+            this.labelEAX1EAX20_27_ExtendedFamilyId.Location = new System.Drawing.Point(6, 334);
+            this.labelEAX1EAX20_27_ExtendedFamilyId.Name = "labelEAX1EAX20_27_ExtendedFamilyId";
+            this.labelEAX1EAX20_27_ExtendedFamilyId.Size = new System.Drawing.Size(101, 13);
+            this.labelEAX1EAX20_27_ExtendedFamilyId.TabIndex = 27;
+            this.labelEAX1EAX20_27_ExtendedFamilyId.Text = "Extended Family ID:";
+            // 
+            // textBoxEAX1EAX20_27_ExtendedFamilyId
+            // 
+            this.textBoxEAX1EAX20_27_ExtendedFamilyId.Location = new System.Drawing.Point(170, 331);
+            this.textBoxEAX1EAX20_27_ExtendedFamilyId.Name = "textBoxEAX1EAX20_27_ExtendedFamilyId";
+            this.textBoxEAX1EAX20_27_ExtendedFamilyId.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX20_27_ExtendedFamilyId.TabIndex = 26;
+            // 
+            // labelEAX1EAX28_31_Reserved
+            // 
+            this.labelEAX1EAX28_31_Reserved.AutoSize = true;
+            this.labelEAX1EAX28_31_Reserved.Location = new System.Drawing.Point(6, 360);
+            this.labelEAX1EAX28_31_Reserved.Name = "labelEAX1EAX28_31_Reserved";
+            this.labelEAX1EAX28_31_Reserved.Size = new System.Drawing.Size(56, 13);
+            this.labelEAX1EAX28_31_Reserved.TabIndex = 29;
+            this.labelEAX1EAX28_31_Reserved.Text = "Reserved:";
+            // 
+            // textBoxEAX1EAX28_31_Reserved
+            // 
+            this.textBoxEAX1EAX28_31_Reserved.Location = new System.Drawing.Point(170, 357);
+            this.textBoxEAX1EAX28_31_Reserved.Name = "textBoxEAX1EAX28_31_Reserved";
+            this.textBoxEAX1EAX28_31_Reserved.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EAX28_31_Reserved.TabIndex = 28;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(609, 162);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 30;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // labelEAX1EBX0_7_BrandIndex
+            // 
+            this.labelEAX1EBX0_7_BrandIndex.AutoSize = true;
+            this.labelEAX1EBX0_7_BrandIndex.Location = new System.Drawing.Point(6, 386);
+            this.labelEAX1EBX0_7_BrandIndex.Name = "labelEAX1EBX0_7_BrandIndex";
+            this.labelEAX1EBX0_7_BrandIndex.Size = new System.Drawing.Size(67, 13);
+            this.labelEAX1EBX0_7_BrandIndex.TabIndex = 32;
+            this.labelEAX1EBX0_7_BrandIndex.Text = "Brand Index:";
+            // 
+            // textBoxEAX1EBX0_7_BrandIndex
+            // 
+            this.textBoxEAX1EBX0_7_BrandIndex.Location = new System.Drawing.Point(170, 383);
+            this.textBoxEAX1EBX0_7_BrandIndex.Name = "textBoxEAX1EBX0_7_BrandIndex";
+            this.textBoxEAX1EBX0_7_BrandIndex.Size = new System.Drawing.Size(200, 20);
+            this.textBoxEAX1EBX0_7_BrandIndex.TabIndex = 31;
+            // 
             // EAX1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -168,5 +380,26 @@
         private System.Windows.Forms.Label labelEAX1EAX;
         private System.Windows.Forms.Label labelEAX1EAX0_3_SteppingId;
         private System.Windows.Forms.TextBox textBoxEAX1EAX0_3_SteppingId;
+        private System.Windows.Forms.Label labelEAX1EAX4_7_ModelId;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX4_7_ModelId;
+        private System.Windows.Forms.Label labelEAX1EAX8_11_FamilyId;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX8_11_FamilyId;
+        private System.Windows.Forms.Label labelEAX1EAX12_13_ProcessorType;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX12_13_ProcessorType;
+        private System.Windows.Forms.Label labelEAX1EAX14_15_Reserved;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX14_15_Reserved;
+        private System.Windows.Forms.Label labelEAX1EAX16_19_ExtendedModelId;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX16_19_ExtendedModelId;
+        private System.Windows.Forms.Label labelEAX1EAX16_19_ExtendedModelIdLeftShifted;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted;
+        private System.Windows.Forms.Label labelEAX1EAX_CalculatedProcessorModel;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX_CalculatedProcessorModel;
+        private System.Windows.Forms.Label labelEAX1EAX20_27_ExtendedFamilyId;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX20_27_ExtendedFamilyId;
+        private System.Windows.Forms.Label labelEAX1EAX28_31_Reserved;
+        private System.Windows.Forms.TextBox textBoxEAX1EAX28_31_Reserved;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label labelEAX1EBX0_7_BrandIndex;
+        private System.Windows.Forms.TextBox textBoxEAX1EBX0_7_BrandIndex;
     }
 }
--- CPU-S/EAX1.cs
+++ CPU-S/EAX1.cs
@@ -38,6 +38,36 @@ namespace CPU_S
             string cpuIdEAX1EAX0_3_SteppingId = cpuHelper.GetEAX1EAX0_3_SteppingIdX();
             textBoxEAX1EAX0_3_SteppingId.Text = cpuIdEAX1EAX0_3_SteppingId;
 
+            string cpuIdEAX1EAX4_7_ModelId = cpuHelper.GetEAX1EAX4_7_ModelIdX();
+            textBoxEAX1EAX4_7_ModelId.Text = cpuIdEAX1EAX4_7_ModelId;
+
+            string cpuIdEAX1EAX8_11_FamilyId = cpuHelper.GetEAX1EAX8_11_FamilyIdX();
+            textBoxEAX1EAX8_11_FamilyId.Text = cpuIdEAX1EAX8_11_FamilyId;
+
+            string cpuIdEAX1EAX12_13_ProcessorType = cpuHelper.GetEAX1EAX12_13_ProcessorTypeX();
+            textBoxEAX1EAX12_13_ProcessorType.Text = cpuIdEAX1EAX12_13_ProcessorType;
+
+            string cpuIdEAX1EAX14_15_Reserved = cpuHelper.GetEAX1EAX14_15_ReservedX();
+            textBoxEAX1EAX14_15_Reserved.Text = cpuIdEAX1EAX14_15_Reserved;
+
+            string cpuIdEAX1EAX16_19_ExtendedModelId = cpuHelper.GetEAX1EAX16_19_ExtendedModelIdX();
+            textBoxEAX1EAX16_19_ExtendedModelId.Text = cpuIdEAX1EAX16_19_ExtendedModelId;
+
+            string cpuIdEAX1EAX16_19_ExtendedModelIdLeftShifted = cpuHelper.GetEAX1EAX16_19_ExtendedModelIdLeftShiftedX();
+            textBoxEAX1EAX16_19_ExtendedModelIdLeftShifted.Text = cpuIdEAX1EAX16_19_ExtendedModelIdLeftShifted;
+
+            string cpuIdEAX1EAX_CalculatedProcessorModel = (int.Parse(cpuIdEAX1EAX4_7_ModelId) + int.Parse(cpuIdEAX1EAX16_19_ExtendedModelIdLeftShifted)).ToString();
+            textBoxEAX1EAX_CalculatedProcessorModel.Text = cpuIdEAX1EAX_CalculatedProcessorModel;
+
+            string cpuIdEAX1EAX20_27_ExtendedFamilyId = cpuHelper.GetEAX1EAX20_27_ExtendedFamilyIdX();
+            textBoxEAX1EAX20_27_ExtendedFamilyId.Text = cpuIdEAX1EAX20_27_ExtendedFamilyId;
+
+            string cpuIdEAX1EAX28_31_Reserved = cpuHelper.GetEAX1EAX28_31_ReservedX();
+            textBoxEAX1EAX28_31_Reserved.Text = cpuIdEAX1EAX28_31_Reserved;
+
+            string cpuIdEAX1EBX0_7_BrandIndex = cpuHelper.GetEAX1EBX0_7_BrandIndexX();
+            textBoxEAX1EBX0_7_BrandIndex.Text = cpuIdEAX1EBX0_7_BrandIndex;
+
             #endregion
         }
     }
