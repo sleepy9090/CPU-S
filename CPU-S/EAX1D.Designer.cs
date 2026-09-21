72d43b65f471d26a6fbbdda69d9ac099d75c4aec
sleepy9090 <sleepy3d@gmail.com>
2026-09-21 11:43:35 -0400

    EAX=0x1D: Intel AMX Tile Information

--- CPU-S/EAX1D.Designer.cs
+++ CPU-S/EAX1D.Designer.cs
@@ -38,12 +38,31 @@
             this.labelEAX1DEBX = new System.Windows.Forms.Label();
             this.labelEAX1DECX = new System.Windows.Forms.Label();
             this.labelEAX1DEDX = new System.Windows.Forms.Label();
+            this.groupBoxEAX = new System.Windows.Forms.GroupBox();
+            this.groupBoxEBX = new System.Windows.Forms.GroupBox();
+            this.groupBoxECX = new System.Windows.Forms.GroupBox();
+            this.textBoxTotalTileBytes = new System.Windows.Forms.TextBox();
+            this.textBoxBytesPerTile = new System.Windows.Forms.TextBox();
+            this.labelTotalTileBytes = new System.Windows.Forms.Label();
+            this.labelBytesPerTile = new System.Windows.Forms.Label();
+            this.textBoxBytesPerRow = new System.Windows.Forms.TextBox();
+            this.textBoxMaxNames = new System.Windows.Forms.TextBox();
+            this.labelBytesPerRow = new System.Windows.Forms.Label();
+            this.labelMaxNames = new System.Windows.Forms.Label();
+            this.textBoxMaxRows = new System.Windows.Forms.TextBox();
+            this.labelMaxRows = new System.Windows.Forms.Label();
             this.groupBoxEAX1D.SuspendLayout();
             this.groupBoxAll.SuspendLayout();
+            this.groupBoxEAX.SuspendLayout();
+            this.groupBoxEBX.SuspendLayout();
+            this.groupBoxECX.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxEAX1D
             // 
+            this.groupBoxEAX1D.Controls.Add(this.groupBoxECX);
+            this.groupBoxEAX1D.Controls.Add(this.groupBoxEBX);
+            this.groupBoxEAX1D.Controls.Add(this.groupBoxEAX);
             this.groupBoxEAX1D.Controls.Add(this.groupBoxAll);
             this.groupBoxEAX1D.Location = new System.Drawing.Point(12, 12);
             this.groupBoxEAX1D.Name = "groupBoxEAX1D";
@@ -132,6 +151,123 @@
             this.labelEAX1DEDX.TabIndex = 9;
             this.labelEAX1DEDX.Text = "EDX:";
             // 
+            // groupBoxEAX
+            // 
+            this.groupBoxEAX.Controls.Add(this.labelBytesPerTile);
+            this.groupBoxEAX.Controls.Add(this.labelTotalTileBytes);
+            this.groupBoxEAX.Controls.Add(this.textBoxBytesPerTile);
+            this.groupBoxEAX.Controls.Add(this.textBoxTotalTileBytes);
+            this.groupBoxEAX.Location = new System.Drawing.Point(6, 158);
+            this.groupBoxEAX.Name = "groupBoxEAX";
+            this.groupBoxEAX.Size = new System.Drawing.Size(419, 81);
+            this.groupBoxEAX.TabIndex = 41;
+            this.groupBoxEAX.TabStop = false;
+            this.groupBoxEAX.Text = "EAX";
+            // 
+            // groupBoxEBX
+            // 
+            this.groupBoxEBX.Controls.Add(this.labelMaxNames);
+            this.groupBoxEBX.Controls.Add(this.labelBytesPerRow);
+            this.groupBoxEBX.Controls.Add(this.textBoxMaxNames);
+            this.groupBoxEBX.Controls.Add(this.textBoxBytesPerRow);
+            this.groupBoxEBX.Location = new System.Drawing.Point(6, 245);
+            this.groupBoxEBX.Name = "groupBoxEBX";
+            this.groupBoxEBX.Size = new System.Drawing.Size(419, 79);
+            this.groupBoxEBX.TabIndex = 42;
+            this.groupBoxEBX.TabStop = false;
+            this.groupBoxEBX.Text = "EBX";
+            // 
+            // groupBoxECX
+            // 
+            this.groupBoxECX.Controls.Add(this.labelMaxRows);
+            this.groupBoxECX.Controls.Add(this.textBoxMaxRows);
+            this.groupBoxECX.Location = new System.Drawing.Point(6, 330);
+            this.groupBoxECX.Name = "groupBoxECX";
+            this.groupBoxECX.Size = new System.Drawing.Size(419, 53);
+            this.groupBoxECX.TabIndex = 43;
+            this.groupBoxECX.TabStop = false;
+            this.groupBoxECX.Text = "ECX";
+            // 
+            // textBoxTotalTileBytes
+            // 
+            this.textBoxTotalTileBytes.Location = new System.Drawing.Point(309, 19);
+            this.textBoxTotalTileBytes.Name = "textBoxTotalTileBytes";
+            this.textBoxTotalTileBytes.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTotalTileBytes.TabIndex = 0;
+            // 
+            // textBoxBytesPerTile
+            // 
+            this.textBoxBytesPerTile.Location = new System.Drawing.Point(309, 45);
+            this.textBoxBytesPerTile.Name = "textBoxBytesPerTile";
+            this.textBoxBytesPerTile.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBytesPerTile.TabIndex = 1;
+            // 
+            // labelTotalTileBytes
+            // 
+            this.labelTotalTileBytes.AutoSize = true;
+            this.labelTotalTileBytes.Location = new System.Drawing.Point(6, 22);
+            this.labelTotalTileBytes.Name = "labelTotalTileBytes";
+            this.labelTotalTileBytes.Size = new System.Drawing.Size(297, 13);
+            this.labelTotalTileBytes.TabIndex = 2;
+            this.labelTotalTileBytes.Text = "total_tile_bytes - Size of all tile registers, in bytes (8092) [0-15]:";
+            // 
+            // labelBytesPerTile
+            // 
+            this.labelBytesPerTile.AutoSize = true;
+            this.labelBytesPerTile.Location = new System.Drawing.Point(6, 48);
+            this.labelBytesPerTile.Name = "labelBytesPerTile";
+            this.labelBytesPerTile.Size = new System.Drawing.Size(264, 13);
+            this.labelBytesPerTile.TabIndex = 3;
+            this.labelBytesPerTile.Text = "bytes_per_tile - Size of one tile, in bytes (1024) [16-31]:";
+            // 
+            // textBoxBytesPerRow
+            // 
+            this.textBoxBytesPerRow.Location = new System.Drawing.Point(309, 19);
+            this.textBoxBytesPerRow.Name = "textBoxBytesPerRow";
+            this.textBoxBytesPerRow.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBytesPerRow.TabIndex = 0;
+            // 
+            // textBoxMaxNames
+            // 
+            this.textBoxMaxNames.Location = new System.Drawing.Point(309, 45);
+            this.textBoxMaxNames.Name = "textBoxMaxNames";
+            this.textBoxMaxNames.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxNames.TabIndex = 1;
+            // 
+            // labelBytesPerRow
+            // 
+            this.labelBytesPerRow.AutoSize = true;
+            this.labelBytesPerRow.Location = new System.Drawing.Point(6, 22);
+            this.labelBytesPerRow.Name = "labelBytesPerRow";
+            this.labelBytesPerRow.Size = new System.Drawing.Size(136, 13);
+            this.labelBytesPerRow.TabIndex = 2;
+            this.labelBytesPerRow.Text = "bytes_per_row - (64) [0-15]:";
+            // 
+            // labelMaxNames
+            // 
+            this.labelMaxNames.AutoSize = true;
+            this.labelMaxNames.Location = new System.Drawing.Point(6, 48);
+            this.labelMaxNames.Name = "labelMaxNames";
+            this.labelMaxNames.Size = new System.Drawing.Size(233, 13);
+            this.labelMaxNames.TabIndex = 3;
+            this.labelMaxNames.Text = "max_names - Number of tile registers (8) [16-31]:";
+            // 
+            // textBoxMaxRows
+            // 
+            this.textBoxMaxRows.Location = new System.Drawing.Point(309, 19);
+            this.textBoxMaxRows.Name = "textBoxMaxRows";
+            this.textBoxMaxRows.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxRows.TabIndex = 0;
+            // 
+            // labelMaxRows
+            // 
+            this.labelMaxRows.AutoSize = true;
+            this.labelMaxRows.Location = new System.Drawing.Point(6, 22);
+            this.labelMaxRows.Name = "labelMaxRows";
+            this.labelMaxRows.Size = new System.Drawing.Size(114, 13);
+            this.labelMaxRows.TabIndex = 1;
+            this.labelMaxRows.Text = "max_rows - (16) [0-15]:";
+            // 
             // EAX1D
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -145,6 +281,12 @@
             this.groupBoxEAX1D.ResumeLayout(false);
             this.groupBoxAll.ResumeLayout(false);
             this.groupBoxAll.PerformLayout();
+            this.groupBoxEAX.ResumeLayout(false);
+            this.groupBoxEAX.PerformLayout();
+            this.groupBoxEBX.ResumeLayout(false);
+            this.groupBoxEBX.PerformLayout();
+            this.groupBoxECX.ResumeLayout(false);
+            this.groupBoxECX.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -161,5 +303,18 @@
         private System.Windows.Forms.Label labelEAX1DEBX;
         private System.Windows.Forms.Label labelEAX1DECX;
         private System.Windows.Forms.Label labelEAX1DEDX;
+        private System.Windows.Forms.GroupBox groupBoxECX;
+        private System.Windows.Forms.GroupBox groupBoxEBX;
+        private System.Windows.Forms.GroupBox groupBoxEAX;
+        private System.Windows.Forms.Label labelBytesPerTile;
+        private System.Windows.Forms.Label labelTotalTileBytes;
+        private System.Windows.Forms.TextBox textBoxBytesPerTile;
+        private System.Windows.Forms.TextBox textBoxTotalTileBytes;
+        private System.Windows.Forms.Label labelMaxNames;
+        private System.Windows.Forms.Label labelBytesPerRow;
+        private System.Windows.Forms.TextBox textBoxMaxNames;
+        private System.Windows.Forms.TextBox textBoxBytesPerRow;
+        private System.Windows.Forms.Label labelMaxRows;
+        private System.Windows.Forms.TextBox textBoxMaxRows;
     }
 }
--- CPU-S/EAX1D.cs
+++ CPU-S/EAX1D.cs
@@ -34,6 +34,21 @@ namespace CPU_S
             string cpuIdEAX1DEDX = cpuHelper.GetEAX1DEDXX();
             textBoxEAX1DEDX.Text = cpuIdEAX1DEDX;
 
+            string cpuIdEAX1DEAX0_15_TotalTileBytes = cpuHelper.GetEAX1DEAX0_15_TotalTileBytesX();
+            textBoxTotalTileBytes.Text = cpuIdEAX1DEAX0_15_TotalTileBytes;
+
+            string cpuIdEAX1DEAX16_31_BytesPerTile = cpuHelper.GetEAX1DEAX16_31_BytesPerTileX();
+            textBoxBytesPerTile.Text = cpuIdEAX1DEAX16_31_BytesPerTile;
+
+            string cpuIdEAX1DEAX0_15_BytesPerRow = cpuHelper.GetEAX1DEAX0_15_BytesPerRowX();
+            textBoxBytesPerRow.Text = cpuIdEAX1DEAX0_15_BytesPerRow;
+
+            string cpuIdEAX1DEAX16_31_MaxNames = cpuHelper.GetEAX1DEAX16_31_MaxNamesX();
+            textBoxMaxNames.Text = cpuIdEAX1DEAX16_31_MaxNames;
+
+            string cpuIdEAX1DEAX16_31_MaxRows = cpuHelper.GetEAX1DEAX0_15_MaxRowsX();
+            textBoxMaxRows.Text = cpuIdEAX1DEAX16_31_MaxRows;
+
             #endregion
         }
     }
