0198f03b893b2499de27f4973c08881aa3092e00
sleepy9090 <sleepy3d@gmail.com>
2026-09-21 12:08:15 -0400

    EAX=0x1E: Intel AMX Tile Multiplier (TMUL) Information

--- CPU-S/EAX1E.Designer.cs
+++ CPU-S/EAX1E.Designer.cs
@@ -38,12 +38,31 @@
             this.labelEAX1EBX = new System.Windows.Forms.Label();
             this.labelEAX1ECX = new System.Windows.Forms.Label();
             this.labelEAX1EDX = new System.Windows.Forms.Label();
+            this.groupBoxEAXECX0 = new System.Windows.Forms.GroupBox();
+            this.groupBoxEAXECX1 = new System.Windows.Forms.GroupBox();
+            this.textBoxTmulMaxk = new System.Windows.Forms.TextBox();
+            this.textBoxTmulMaxn = new System.Windows.Forms.TextBox();
+            this.labelTmulMaxk = new System.Windows.Forms.Label();
+            this.labelTmulMaxn = new System.Windows.Forms.Label();
+            this.checkBoxAmxInt8 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxBf16 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxComplex = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxFp16 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxFp8 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxTranspose = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxTf32 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxAvx512 = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmxMovrs = new System.Windows.Forms.CheckBox();
             this.groupBoxEAX1E.SuspendLayout();
             this.groupBoxAll.SuspendLayout();
+            this.groupBoxEAXECX0.SuspendLayout();
+            this.groupBoxEAXECX1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxEAX1E
             // 
+            this.groupBoxEAX1E.Controls.Add(this.groupBoxEAXECX1);
+            this.groupBoxEAX1E.Controls.Add(this.groupBoxEAXECX0);
             this.groupBoxEAX1E.Controls.Add(this.groupBoxAll);
             this.groupBoxEAX1E.Location = new System.Drawing.Point(12, 12);
             this.groupBoxEAX1E.Name = "groupBoxEAX1E";
@@ -132,6 +151,159 @@
             this.labelEAX1EDX.TabIndex = 9;
             this.labelEAX1EDX.Text = "EDX:";
             // 
+            // groupBoxEAXECX0
+            // 
+            this.groupBoxEAXECX0.Controls.Add(this.labelTmulMaxn);
+            this.groupBoxEAXECX0.Controls.Add(this.labelTmulMaxk);
+            this.groupBoxEAXECX0.Controls.Add(this.textBoxTmulMaxn);
+            this.groupBoxEAXECX0.Controls.Add(this.textBoxTmulMaxk);
+            this.groupBoxEAXECX0.Location = new System.Drawing.Point(264, 19);
+            this.groupBoxEAXECX0.Name = "groupBoxEAXECX0";
+            this.groupBoxEAXECX0.Size = new System.Drawing.Size(435, 79);
+            this.groupBoxEAXECX0.TabIndex = 41;
+            this.groupBoxEAXECX0.TabStop = false;
+            this.groupBoxEAXECX0.Text = "EAX (ECX=0)";
+            // 
+            // groupBoxEAXECX1
+            // 
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxMovrs);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxAvx512);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxTf32);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxTranspose);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxFp8);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxFp16);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxComplex);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxBf16);
+            this.groupBoxEAXECX1.Controls.Add(this.checkBoxAmxInt8);
+            this.groupBoxEAXECX1.Location = new System.Drawing.Point(264, 104);
+            this.groupBoxEAXECX1.Name = "groupBoxEAXECX1";
+            this.groupBoxEAXECX1.Size = new System.Drawing.Size(265, 232);
+            this.groupBoxEAXECX1.TabIndex = 42;
+            this.groupBoxEAXECX1.TabStop = false;
+            this.groupBoxEAXECX1.Text = "EAX (ECX=1)";
+            // 
+            // textBoxTmulMaxk
+            // 
+            this.textBoxTmulMaxk.Location = new System.Drawing.Point(326, 19);
+            this.textBoxTmulMaxk.Name = "textBoxTmulMaxk";
+            this.textBoxTmulMaxk.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTmulMaxk.TabIndex = 0;
+            // 
+            // textBoxTmulMaxn
+            // 
+            this.textBoxTmulMaxn.Location = new System.Drawing.Point(326, 45);
+            this.textBoxTmulMaxn.Name = "textBoxTmulMaxn";
+            this.textBoxTmulMaxn.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTmulMaxn.TabIndex = 1;
+            // 
+            // labelTmulMaxk
+            // 
+            this.labelTmulMaxk.AutoSize = true;
+            this.labelTmulMaxk.Location = new System.Drawing.Point(6, 22);
+            this.labelTmulMaxk.Name = "labelTmulMaxk";
+            this.labelTmulMaxk.Size = new System.Drawing.Size(303, 13);
+            this.labelTmulMaxk.TabIndex = 2;
+            this.labelTmulMaxk.Text = "TMUL_MAXK - Maximum number of rows or columns (16) [0-7]:";
+            // 
+            // labelTmulMaxn
+            // 
+            this.labelTmulMaxn.AutoSize = true;
+            this.labelTmulMaxn.Location = new System.Drawing.Point(6, 48);
+            this.labelTmulMaxn.Name = "labelTmulMaxn";
+            this.labelTmulMaxn.Size = new System.Drawing.Size(314, 13);
+            this.labelTmulMaxn.TabIndex = 3;
+            this.labelTmulMaxn.Text = "TMUL_MAXN - Maximum number of bytes per column (64) [8-23]:";
+            // 
+            // checkBoxAmxInt8
+            // 
+            this.checkBoxAmxInt8.AutoSize = true;
+            this.checkBoxAmxInt8.Location = new System.Drawing.Point(9, 19);
+            this.checkBoxAmxInt8.Name = "checkBoxAmxInt8";
+            this.checkBoxAmxInt8.Size = new System.Drawing.Size(193, 17);
+            this.checkBoxAmxInt8.TabIndex = 0;
+            this.checkBoxAmxInt8.Text = "AMX-INT8 - 8-bit integer support [0]";
+            this.checkBoxAmxInt8.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxBf16
+            // 
+            this.checkBoxAmxBf16.AutoSize = true;
+            this.checkBoxAmxBf16.Location = new System.Drawing.Point(9, 42);
+            this.checkBoxAmxBf16.Name = "checkBoxAmxBf16";
+            this.checkBoxAmxBf16.Size = new System.Drawing.Size(165, 17);
+            this.checkBoxAmxBf16.TabIndex = 1;
+            this.checkBoxAmxBf16.Text = "AMX-BF16 - bfloat support [1]";
+            this.checkBoxAmxBf16.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxComplex
+            // 
+            this.checkBoxAmxComplex.AutoSize = true;
+            this.checkBoxAmxComplex.Location = new System.Drawing.Point(9, 65);
+            this.checkBoxAmxComplex.Name = "checkBoxAmxComplex";
+            this.checkBoxAmxComplex.Size = new System.Drawing.Size(243, 17);
+            this.checkBoxAmxComplex.TabIndex = 2;
+            this.checkBoxAmxComplex.Text = "AMX-COMPLEX - Complex number support [2]";
+            this.checkBoxAmxComplex.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxFp16
+            // 
+            this.checkBoxAmxFp16.AutoSize = true;
+            this.checkBoxAmxFp16.Location = new System.Drawing.Point(9, 88);
+            this.checkBoxAmxFp16.Name = "checkBoxAmxFp16";
+            this.checkBoxAmxFp16.Size = new System.Drawing.Size(171, 17);
+            this.checkBoxAmxFp16.TabIndex = 3;
+            this.checkBoxAmxFp16.Text = "AMX-FP16 - float16 support [3]";
+            this.checkBoxAmxFp16.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxFp8
+            // 
+            this.checkBoxAmxFp8.AutoSize = true;
+            this.checkBoxAmxFp8.Location = new System.Drawing.Point(9, 111);
+            this.checkBoxAmxFp8.Name = "checkBoxAmxFp8";
+            this.checkBoxAmxFp8.Size = new System.Drawing.Size(159, 17);
+            this.checkBoxAmxFp8.TabIndex = 4;
+            this.checkBoxAmxFp8.Text = "AMX-FP8 - float8 support [4]";
+            this.checkBoxAmxFp8.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxTranspose
+            // 
+            this.checkBoxAmxTranspose.AutoSize = true;
+            this.checkBoxAmxTranspose.Location = new System.Drawing.Point(9, 134);
+            this.checkBoxAmxTranspose.Name = "checkBoxAmxTranspose";
+            this.checkBoxAmxTranspose.Size = new System.Drawing.Size(243, 17);
+            this.checkBoxAmxTranspose.TabIndex = 5;
+            this.checkBoxAmxTranspose.Text = "AMX-TRANSPOSE - Transposition support [5]";
+            this.checkBoxAmxTranspose.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxTf32
+            // 
+            this.checkBoxAmxTf32.AutoSize = true;
+            this.checkBoxAmxTf32.Location = new System.Drawing.Point(9, 157);
+            this.checkBoxAmxTf32.Name = "checkBoxAmxTf32";
+            this.checkBoxAmxTf32.Size = new System.Drawing.Size(254, 17);
+            this.checkBoxAmxTf32.TabIndex = 6;
+            this.checkBoxAmxTf32.Text = "AMX-TF32 - TensorFloat-32 (\"FP19\") support [6]";
+            this.checkBoxAmxTf32.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxAvx512
+            // 
+            this.checkBoxAmxAvx512.AutoSize = true;
+            this.checkBoxAmxAvx512.Location = new System.Drawing.Point(9, 180);
+            this.checkBoxAmxAvx512.Name = "checkBoxAmxAvx512";
+            this.checkBoxAmxAvx512.Size = new System.Drawing.Size(218, 17);
+            this.checkBoxAmxAvx512.TabIndex = 7;
+            this.checkBoxAmxAvx512.Text = "AMX-AVX512 - AMX-AVX512 support [7]";
+            this.checkBoxAmxAvx512.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmxMovrs
+            // 
+            this.checkBoxAmxMovrs.AutoSize = true;
+            this.checkBoxAmxMovrs.Location = new System.Drawing.Point(9, 203);
+            this.checkBoxAmxMovrs.Name = "checkBoxAmxMovrs";
+            this.checkBoxAmxMovrs.Size = new System.Drawing.Size(218, 17);
+            this.checkBoxAmxMovrs.TabIndex = 8;
+            this.checkBoxAmxMovrs.Text = "AMX-MOVRS - AMX-MOVRS support [8]";
+            this.checkBoxAmxMovrs.UseVisualStyleBackColor = true;
+            // 
             // EAX1E
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -145,6 +317,10 @@
             this.groupBoxEAX1E.ResumeLayout(false);
             this.groupBoxAll.ResumeLayout(false);
             this.groupBoxAll.PerformLayout();
+            this.groupBoxEAXECX0.ResumeLayout(false);
+            this.groupBoxEAXECX0.PerformLayout();
+            this.groupBoxEAXECX1.ResumeLayout(false);
+            this.groupBoxEAXECX1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -161,5 +337,20 @@
         private System.Windows.Forms.Label labelEAX1EBX;
         private System.Windows.Forms.Label labelEAX1ECX;
         private System.Windows.Forms.Label labelEAX1EDX;
+        private System.Windows.Forms.GroupBox groupBoxEAXECX1;
+        private System.Windows.Forms.GroupBox groupBoxEAXECX0;
+        private System.Windows.Forms.TextBox textBoxTmulMaxk;
+        private System.Windows.Forms.Label labelTmulMaxk;
+        private System.Windows.Forms.TextBox textBoxTmulMaxn;
+        private System.Windows.Forms.Label labelTmulMaxn;
+        private System.Windows.Forms.CheckBox checkBoxAmxBf16;
+        private System.Windows.Forms.CheckBox checkBoxAmxInt8;
+        private System.Windows.Forms.CheckBox checkBoxAmxMovrs;
+        private System.Windows.Forms.CheckBox checkBoxAmxAvx512;
+        private System.Windows.Forms.CheckBox checkBoxAmxTf32;
+        private System.Windows.Forms.CheckBox checkBoxAmxTranspose;
+        private System.Windows.Forms.CheckBox checkBoxAmxFp8;
+        private System.Windows.Forms.CheckBox checkBoxAmxFp16;
+        private System.Windows.Forms.CheckBox checkBoxAmxComplex;
     }
 }
--- CPU-S/EAX1E.cs
+++ CPU-S/EAX1E.cs
@@ -34,6 +34,39 @@ namespace CPU_S
             string cpuIdEAX1EEDX = cpuHelper.GetEAX1EEDXX();
             textBoxEAX1EDX.Text = cpuIdEAX1EEDX;
 
+            string cpuIdEAX1EEAX0_7_MaxNumRowsOrCols = cpuHelper.GetEAX1EEAX0_7_MaxNumRowsOrColsX();
+            textBoxTmulMaxk.Text = cpuIdEAX1EEAX0_7_MaxNumRowsOrCols;
+
+            string cpuIdEAX1EEAX8_23_MaxNumBytesPerCol = cpuHelper.GetEAX1EEAX8_23_MaxNumBytesPerColX();
+            textBoxTmulMaxn.Text = cpuIdEAX1EEAX8_23_MaxNumBytesPerCol;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXINT8IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXINT8IsSupportedX();
+            checkBoxAmxInt8.Checked = cpuIdEAX1EECX1_EAX0_AMXINT8IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXBF16IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXBF16IsSupportedX();
+            checkBoxAmxBf16.Checked = cpuIdEAX1EECX1_EAX0_AMXBF16IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXComplexIsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXComplexIsSupportedX();
+            checkBoxAmxComplex.Checked = cpuIdEAX1EECX1_EAX0_AMXComplexIsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXFP16IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXFP16IsSupportedX();
+            checkBoxAmxFp16.Checked = cpuIdEAX1EECX1_EAX0_AMXFP16IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXFP8IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXFP8IsSupportedX();
+            checkBoxAmxFp8.Checked = cpuIdEAX1EECX1_EAX0_AMXFP8IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXTransposeIsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXTransposeIsSupportedX();
+            checkBoxAmxTranspose.Checked = cpuIdEAX1EECX1_EAX0_AMXTransposeIsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXTF32IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXTF32IsSupportedX();
+            checkBoxAmxTf32.Checked = cpuIdEAX1EECX1_EAX0_AMXTF32IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXAVX512IsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXAVX512IsSupportedX();
+            checkBoxAmxAvx512.Checked = cpuIdEAX1EECX1_EAX0_AMXAVX512IsSupported;
+
+            bool cpuIdEAX1EECX1_EAX0_AMXMOVRSIsSupported = cpuHelper.GetEAX1EECX1_EAX0_AMXMOVRSIsSupportedX();
+            checkBoxAmxMovrs.Checked = cpuIdEAX1EECX1_EAX0_AMXMOVRSIsSupported;
+
             #endregion
         }
     }
